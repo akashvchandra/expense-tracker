18b13d36ae906ddb2fc86df60d8c7b20302feb2e
akashvchandra <akashvchandra@gmail.com>
2023-07-24 14:36:23 +0530

    add existing project to git

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,22 @@
-const express = require('express')
-const cors = require('cors');
-const { db } = require('./db/db');
-const {readdirSync} = require('fs')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { db } = require("./db/db");
+const { readdirSync } = require("fs");
+const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cors());
 
-//middlewares
-app.use(express.json())
-app.use(cors())
-
-//routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+readdirSync("./routes").map((route) => app.use("/api/v1", require("./routes/" + route)));
 
 const server = () => {
-    db()
+    db();
     app.listen(PORT, () => {
-        console.log('listening to port:', PORT)
-    })
-}
+        console.log("listening to port:", PORT);
+    });
+};
 
-server()
+server();
